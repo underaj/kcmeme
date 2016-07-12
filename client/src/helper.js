0f5e1b79016049f593112c642d683802bd8e5665
Ching <chingyang91@gmail.com>
2016-07-12 00:04:44 -0700

    fix api get and post request to point at localhost

--- client/src/helper.js
+++ client/src/helper.js
@@ -1,12 +1,12 @@
 export var apiGet = function apiGet (path, callback) {
-  var jqXHR = $.get('http://localhost:8080' + path);
+  var jqXHR = $.get(path);
   jqXHR.done(data => callback(data));
 };
 
 export var apiPost = function apiPost (path, postObj) {
   return $.ajax({
     method: 'POST',
-    url: 'http://localhost:8080' + path,
+    url: path,
     data: JSON.stringify(postObj),
     contentType: 'application/json'
   });
