d92f624dab317fcb728e42aa219d1d30c0ef6a3d
Ching <chingyang91@gmail.com>
2016-07-11 22:56:25 -0700

    mongoLab uri

--- server/configs/mongoose.js
+++ server/configs/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 module.exports = function() {
-  mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/protips');
+  mongoose.connect('mongodb://heroku_l6g6p74f:bl8namrrgacu72qft9d0vs59j6@ds017514.mlab.com:17514/heroku_l6g6p74f' || 'mongodb://localhost/protips');
   var db = mongoose.connection;
 
   db.on('error', function (err) {
